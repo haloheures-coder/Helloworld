b407348036f70fa68708d9ebe9b973b802857747
haloheures-coder <halo.heures@gmail.com>
2026-06-24 11:21:42 +0000

    helloworld

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# 1. 화면에 "Hello, World!"를 출력하는 함수 만들기
+def say_hello():
+    print("Hello, World!")  # <- 이 줄을 Copilot이 자동으로 제안합니다.
+
+
+# 2. 위에서 만든 함수를 호출하기
+say_hello()  # <- 이 줄도 Copilot이 문맥을 파악하고 자동으로 제안합니다.
